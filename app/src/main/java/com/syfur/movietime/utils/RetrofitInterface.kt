6489ff05057b7f261e246b397f8cffb6182a851e
Syfur007 <syfur.cse8.bu@gmail.com>
2023-04-03 06:12:48 +0600

    API Endpoints

--- app/src/main/java/com/syfur/movietime/utils/RetrofitInterface.kt
+++ app/src/main/java/com/syfur/movietime/utils/RetrofitInterface.kt
@@ -1,9 +1,32 @@
 package com.syfur.movietime.utils
 
+import com.syfur.movietime.models.MovieModel
+import com.syfur.movietime.responses.MoviePopularResponse
+import com.syfur.movietime.responses.MovieSearchResponse
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface RetrofitInterface {
+
+    @GET("movie/popular")
+    fun getPopular(@Query("api_key") apiKey: String): Call<MoviePopularResponse>
+
+    @GET("search/movie")
+    fun searchMovie(
+        @Query("api_key") apiKey: String,
+        @Query("query") searchQuery: String,
+        @Query("page") page: Int = 1
+    ): Call<MoviePopularResponse>
+
+    @GET("movie/{id}")
+    fun getMovie(
+        @Path("id") movie_id: Int,
+        @Query("api_key") apiKey: String
+    ): Call<MovieModel>
 
 
 
