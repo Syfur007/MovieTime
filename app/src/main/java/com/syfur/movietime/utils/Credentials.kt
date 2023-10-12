b31abfdf7cb43e6d3be76f90e17af349e1bd4db3
Syfur007 <syfur.cse8.bu@gmail.com>
2023-10-13 03:12:54 +0600

    Changed API Key

--- app/src/main/java/com/syfur/movietime/utils/Credentials.kt
+++ app/src/main/java/com/syfur/movietime/utils/Credentials.kt
@@ -5,6 +5,6 @@ class Credentials {
     companion object {
         var baseUrl = "https://api.themoviedb.org/3/"
         val posterUrl = "https://image.tmdb.org/t/p/w500"
-        var apiKey = "7721172f10bfa23893b9607f7616b655"
+        var apiKey = "8884e4d277159fc98a0d6746a5702f44"
     }
 }
